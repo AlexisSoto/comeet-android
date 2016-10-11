19acf0e065e1788df0ef2fe33c1ef66e02c70d3b
Vincebrees <vincent.eti@gmail.com>
2016-10-11 22:54:17 +0200

    Add Bottom Bar Navigation
    Basic Bottom Bar Navigation with fragment
    TODO : Change icons and name

--- app/src/main/java/today/comeet/android/comeet/activity/HomeActivity.java
+++ app/src/main/java/today/comeet/android/comeet/activity/HomeActivity.java
@@ -12,7 +12,9 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.IdRes;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -28,9 +30,17 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.ncapdevi.fragnav.FragNavController;
+import com.roughike.bottombar.BottomBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import today.comeet.android.comeet.R;
-
+import today.comeet.android.comeet.fragment.FirstFragment;
+import today.comeet.android.comeet.fragment.SecondFragment;
+import today.comeet.android.comeet.fragment.ThirdFragment;
+import com.roughike.bottombar.OnMenuTabClickListener;
 
 public class HomeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -46,6 +56,14 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
     FloatingActionButton btn_plus;
     FloatingActionButton bouttonplus;
     FloatingActionButton floatingActionButton1, floatingActionButton2, floatingActionButton3;
+
+    private BottomBar mBottomBar;
+    private FragNavController fragNavController;
+
+    //indices to fragments
+    private final int TAB_FIRST = FragNavController.TAB1;
+    private final int TAB_SECOND = FragNavController.TAB2;
+    private final int TAB_THIRD = FragNavController.TAB3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -147,6 +165,65 @@ public class HomeActivity extends AppCompatActivity implements OnMapReadyCallbac
             // Execute ce code s'il y a les permissions
             //btn_getPosition(this);
         }
+
+
+        //FragNav
+        //list of fragments
+        List<Fragment> fragments = new ArrayList<>(3);
+
+        //add fragments to list
+        fragments.add(FirstFragment.newInstance(0));
+        fragments.add(SecondFragment.newInstance(0));
+        fragments.add(ThirdFragment.newInstance(0));
+
+        //link fragments to container
+        fragNavController = new FragNavController(getSupportFragmentManager(),R.id.container,fragments);
+        //End of FragNav
+
+        //BottomBar menu
+        mBottomBar = BottomBar.attach(this, savedInstanceState);
+        mBottomBar.setItems(R.menu.bottombar_menu);
+        mBottomBar.setOnMenuTabClickListener(new OnMenuTabClickListener() {
+            @Override
+            public void onMenuTabSelected(@IdRes int menuItemId) {
+                //switch between tabs
+                switch (menuItemId) {
+                    case R.id.bottomBarItemOne:
+                        fragNavController.switchTab(TAB_FIRST);
+                        break;
+                    case R.id.bottomBarItemSecond:
+                        fragNavController.switchTab(TAB_SECOND);
+                        break;
+                    case R.id.bottomBarItemThird:
+                        fragNavController.switchTab(TAB_THIRD);
+                        break;
+                }
+            }
+
+            @Override
+            public void onMenuTabReSelected(@IdRes int menuItemId) {
+                if (menuItemId == R.id.bottomBarItemOne) {
+                    fragNavController.clearStack();
+                }
+            }
+        });
+        //End of BottomBar menu
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (fragNavController.getCurrentStack().size() > 1) {
+            fragNavController.pop();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Necessary to restore the BottomBar's state, otherwise we would
+        // lose the current tab on orientation change.
+        mBottomBar.onSaveInstanceState(outState);
     }
 
     @Override
---
+++ app/src/main/java/today/comeet/android/comeet/fragment/FirstFragment.java
@@ -0,0 +1,35 @@
+package today.comeet.android.comeet.fragment;
+
+/**
+ * Created by Vincent on 11/10/2016.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import today.comeet.android.comeet.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+
+    public static FirstFragment newInstance(int instance) {
+        Bundle args = new Bundle();
+        args.putInt("argsInstance", instance);
+        FirstFragment firstFragment = new FirstFragment();
+        firstFragment.setArguments(args);
+        return firstFragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+}
