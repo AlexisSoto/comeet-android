90721dc6c862eb96107017599a1a90969552ce73
Bettyna-Bourcier <bettyna.bourcier@gmail.com>
2016-10-29 00:52:32 +0200

    gestion des amis favoris (api object de facebook)
    Nous utilisons l'api object de Facebook pour stocker les objets custom :
    Favorite friend. Dans l'administration de l'app, voir Open Graph ->
    types d'objets

--- app/src/main/java/today/comeet/android/comeet/activity/AddFriendActivity.java
+++ app/src/main/java/today/comeet/android/comeet/activity/AddFriendActivity.java
@@ -1,11 +1,13 @@
 package today.comeet.android.comeet.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -23,6 +25,7 @@ import org.json.JSONObject;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import today.comeet.android.comeet.R;
@@ -30,6 +33,14 @@ import today.comeet.android.comeet.R;
 public class AddFriendActivity extends AppCompatActivity {
 
     private ListView friendsListView;
+    private List<String> friendIds;
+    private List<String> friendNames;
+    /*
+        in facebook object api, friends are stored as {id: object_id, title: id_facebook_of_the_friend }
+        Here in the Hap map key = id_facebook_of_the_friend and value = object_id
+        object_id is the facebook_id of the object and i needed when we DELETE it
+     */
+    HashMap<String, String> favoriteFriends;
 
 
     @Override
@@ -39,30 +50,35 @@ public class AddFriendActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        friendIds = new ArrayList();
+        friendNames = new ArrayList();
+        favoriteFriends = new HashMap<>();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // we POST a hard coded object (favorite friend)
-                Bundle params = new Bundle();
-                params.putString("object", "{'og:title': 'Truc'}");
-                GraphRequest request = new GraphRequest(
-                    AccessToken.getCurrentAccessToken(),
-                    "me/objects/comeetapplication:favorite_friend",
-                    params,
-                    HttpMethod.POST,
-                    new GraphRequest.Callback() {
-                        @Override
-                        public void onCompleted(GraphResponse response) {
-                            Log.d("Favorite friend", "added" );
-                        }
 
+                SparseBooleanArray checked = friendsListView.getCheckedItemPositions();
+                for (int i = 0; i < friendIds.size(); i++) {
+                    if(checked.get(i) == true ) {
+                        if(favoriteFriends.get(friendIds.get(i)) == null) {
+                            // not already a favorite friend
+                            createFavoriteFriend(friendIds.get(i));
+                        }
+                    } else {
+                        // not checked
+                        if(favoriteFriends.get(friendIds.get(i)) != null) {
+                            // was a favorite friend, so we delete him
+                            deleteFavoriteFriend(favoriteFriends.get(friendIds.get(i)));
+                        }
                     }
-                );
-                request.executeAsync();
+                }
 
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(AddFriendActivity.this, HomeActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -79,28 +95,95 @@ public class AddFriendActivity extends AppCompatActivity {
                 new GraphRequest.GraphJSONArrayCallback() {
                     @Override
                     public void onCompleted(JSONArray array, GraphResponse response) {
-                        List<String> names = new ArrayList<String>();
                         for (int i = 0; i < array.length(); i++) {
                             JSONObject row = null;
                             try {
                                 row = array.getJSONObject(i);
-                                names.add(row.getString("name"));
+                                friendNames.add(row.getString("name"));
+                                friendIds.add(row.getString("id"));
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
 
                         }
+                        // we have the user friends, so now we ask his favorite friends
+                        GraphRequest request = new GraphRequest(
+                                AccessToken.getCurrentAccessToken(),
+                                "me/objects/comeetapplication:favorite_friend",
+                                null,
+                                HttpMethod.GET,
+                                new GraphRequest.Callback() {
+                                    @Override
+                                    public void onCompleted(GraphResponse response) {
+                                        Log.d("Favorite friend", "GET" );
+                                        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(AddFriendActivity.this, android.R.layout.simple_list_item_multiple_choice, friendNames);
+                                        friendsListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
-                        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(AddFriendActivity.this, android.R.layout.simple_list_item_multiple_choice, names);
-                        friendsListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                                        friendsListView.setAdapter(arrayAdapter);
+                                        try {
+                                            JSONArray array = response.getJSONObject().getJSONArray("data");
+                                            JSONObject row = null;
+                                            // for each favorite friend we check the corresponding item
+                                            for (int i = 0; i < array.length(); i++) {
+                                                row = array.getJSONObject(i);
+                                                favoriteFriends.put(row.getString("title"), row.getString("id"));
+                                                int index = friendIds.indexOf(row.getString("title"));
+                                                if(index > -1) {
+                                                    friendsListView.setItemChecked(index, true);
+                                                }
+                                            }
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
 
-                        friendsListView.setAdapter(arrayAdapter);
-
+                                }
+                        );
+                        request.executeAsync();
                     }
                 });
-
         request.executeAsync();
+    }
 
+    private void createFavoriteFriend(String id) {
+        Log.d("Favorite friend", "want to create " + id);
+        Bundle params = new Bundle();
+        params.putString("object", "{'og:title': " + id + "}");
+        GraphRequest request = new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "me/objects/comeetapplication:favorite_friend",
+                params,
+                HttpMethod.POST,
+                new GraphRequest.Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse response) {
+                        Log.d("Favorite friend", "added" );
+                    }
+
+                }
+        );
+        request.executeAsync();
+    }
+
+    private void deleteFavoriteFriend(String id) {
+        // we POST a hard coded object (favorite friend)
+        Log.d("Favorite friend", "want to delete " + id);
+       // Bundle params = new Bundle();
+       // params.putString("object", "{'og:title': " + id + "}");
+        GraphRequest request = new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                id,
+                null,
+                HttpMethod.DELETE,
+                new GraphRequest.Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse response) {
+                        Log.d("Favorite friend", "deleted" );
+                    }
+
+                }
+        );
+        request.executeAsync();
     }
 
 }
